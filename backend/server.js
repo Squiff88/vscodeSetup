d718ec21d49742c9faadbe03e6c3114fc4f7374f
Stefan.M <metodieff.stefan@gmail.com>
2020-07-12 15:39:52 +0200

    added BE workspace file

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+
+//Body parser
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, console.log(`server 🏃 on ${PORT}`));
+
+
+// Global error handler
+server.on('unhandledRejection' , (err , promise) => {
+    console.log(`Error ${err.message}`);
+
+    server.close(() => process.exit(1));
+});
