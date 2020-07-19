562825b68d48eeb38e8d449d208ca089a137d87b
Stefan.M <metodieff.stefan@gmail.com>
2020-07-19 10:57:32 +0200

    fixed typo

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const express = require('express');
-snippet
 
 const app = express();
 
@@ -13,11 +12,3 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, console.log(`server 🏃 on ${PORT}`));
-
-
-// Global error handler
-server.on('unhandledRejection' , (err , promise) => {
-    console.log(`Error ${err.message}`);
-
-    server.close(() => process.exit(1));
-});
